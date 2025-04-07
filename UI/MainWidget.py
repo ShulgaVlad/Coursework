ee43999d0df391bb8d6f810b0fa3ee9b656cdd54
Влад Шульга <shulgavlad2112@gmail.com>
2025-04-07 12:31:51 +0300

    stemming was added. also changed coords of interface

--- UI/MainWidget.py
+++ UI/MainWidget.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QApplication
+from PyQt6.QtCore import Qt
 from AlgorhythmBasedTextAnalysis.TextStatistics import TextStatistics
 from UI.UiComponents import UIComponents
 from UI.AnalysisLogic import AnalysisLogic
@@ -14,8 +15,26 @@ class AnalysisWidget(QWidget):
 
     def initUI(self):
         """Налаштовує графічний інтерфейс."""
+
         self.setWindowTitle("Інтелектуальний аналіз текстів")
-        self.setGeometry(100, 100, 800, 900)
+
+        # Отримуємо розміри екрану
+        screen = QApplication.primaryScreen()
+        screen_geometry = screen.availableGeometry()
+        screen_width = screen_geometry.width()
+        screen_height = screen_geometry.height()
+
+        # Встановлюємо розміри вікна залежно від розміру екрана (наприклад, 70% x 85%)
+        window_width = int(screen_width * 0.7)
+        window_height = int(screen_height * 0.85)
+        self.setGeometry(
+            (screen_width - window_width) // 2,
+            (screen_height - window_height) // 2,
+            window_width,
+            window_height
+        )
+
+        # Оформлення
         self.layout = QVBoxLayout(self)
         self.layout.setSpacing(15)
         self.layout.setContentsMargins(20, 20, 20, 20)
