d47885f99f5dcab62fe2896b5cc9abfb05614d51
Vlad Shulga <shulgavlad2112@gmail.com>
2025-04-17 23:43:42 +0300

    zipf law implemented

--- UI/AnalysisLogic.py
+++ UI/AnalysisLogic.py
@@ -4,17 +4,26 @@ from AlgorhythmBasedTextAnalysis.AlgorhythmTextAnalysis import AlgorhythmTextAna
 from ReadTxtFile import FileLoader
 from AITextAnalysis.TextHighlighter import TextHighlighter
 from AlgorhythmBasedTextAnalysis.LexicalSentimentAnalysis.LexicalSentimentAnalysis import LexicalSentimentAnalysis
+from AlgorhythmBasedTextAnalysis.ZipfAnalyze import ZipfAnalyze
 
 
 class AnalysisLogic:
-    def __init__(self, model, text_stats, text_input, analyze_button, outputs):
+    def __init__(self, model, text_stats, text_input, analyze_button, outputs, widget):
         self.model = model
         self.text_stats = text_stats
         self.text_input = text_input
         self.analyze_button = analyze_button
-        self.result_text_specialization_output, self.result_text_algorhythm_specialization_output, \
-            self.result_main_sentence_output, self.result_text_statistics_output, \
-            self.result_text_tone_output = outputs
+
+        # Розпаковуємо повернені віджети
+        (self.result_text_specialization_output,
+         self.result_text_algorhythm_specialization_output,
+         self.result_main_sentence_output,
+         self.result_text_statistics_output,
+         self.result_text_tone_output,
+         self.result_zipf_image) = outputs
+
+        # Зберігаємо посилання на головний віджет для масштабування зображення
+        self.widget = widget
 
     def load_file(self):
         """Завантажує текст з файлу у поле введення."""
@@ -32,19 +41,29 @@ class AnalysisLogic:
             self.analysis_thread.start()
 
     def update_result(self, main_sentences, specialization):
-        """Оновлює UI з отриманими результатами."""
         text = self.text_input.toPlainText()
         words, top_words = self.text_stats.calculate_text_statistics(text)
+
+        # Алгоритмічний аналіз
         analyzer = AlgorhythmTextAnalysis(top_words)
         detected_specialization = analyzer.run()
 
+        # Аналіз тону
         tone = LexicalSentimentAnalysis.lexical_sentiment_analysis(text)
+
+        # Закон Зіпфа
+        zipf_analyzer = ZipfAnalyze()
+        zipf_pixmap = zipf_analyzer.plot_zipf_law(top_words)
 
         self.result_main_sentence_output.setText(main_sentences)
         self.result_text_specialization_output.setText(specialization)
         self.result_text_statistics_output.setText(self.text_stats.statistics_text)
         self.result_text_algorhythm_specialization_output.setText(detected_specialization)
         self.result_text_tone_output.setText(tone)
+
+        # Відображення графіка
+        if zipf_pixmap:
+            self.result_zipf_image.setPixmap(zipf_pixmap.scaledToWidth(600))
 
         TextHighlighter.highlight_text(self.text_input, text, main_sentences)
         self.analyze_button.setEnabled(True)
